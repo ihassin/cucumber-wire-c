3fd6cc7a170c749d4681f66745fc04d3dcd6756f
Itamar Hassin <ihassin@mac.com>
2014-10-12 10:36:50 -0400

    Fixed memory allocation bug

--- wire-c/wire-server/main.c
+++ wire-c/wire-server/main.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
+
+#ifndef __SYSTEM_INCLUDES_H__
+#include "system-includes.h"
+#endif
 
 #ifndef __WIRE_SERVER_H__
 #include "wire-server.h"
@@ -27,7 +31,9 @@ int main(int argc, char **argv)
 	int ret_val;
 	wire_context *context;
 
-	context = malloc(sizeof(context));
+	context = malloc(sizeof(wire_context));
+	memset(context, 0, sizeof(wire_context));
+
 	context->port 				= atoi(argv[1]);
 	context->single_scenario 	= atoi(argv[2]);
 	context->logger 			= my_logger;
